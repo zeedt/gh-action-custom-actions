c27733df9e687f40b06160d9f0a28b3c22fe8a30
zeed <yusufsaheedtaiwo@gmail.com>
2023-08-17 18:31:13 +0100

    custom js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,18 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    core.notice('Bucket is ' + bucket);
+    core.notice('Bucket Region is ' + bucketRegion);
+    core.notice('Dist FOlder is ' + distFolder);
+
+    // Upload files to S3
+    const S3Uri = `s3://${bucket}`
+    // exec.exec(`aws s3 async ${distFolder} ${bucket} --region ${bucketRegion}`)
+
     core.notice('Hello from my Javascript custom action');
 }
 
